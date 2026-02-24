c22f1801c7ce379cdb7001a3afef87b718e0eb78
Alexander <flamestrike02@gmail.com>
2026-02-24 14:16:23 -0300

    Se configuran los archivos build.gradle con las librerías necesarias para el manejo de estado, inyección de dependencias y networking (PokéAPI). Se establece el tema base con los colores del sistema.

---
+++ app/build.gradle.kts
@@ -0,0 +1,99 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.jetbrainsKotlinSerialization)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.amartinez.pokeapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.amartinez.pokeapp"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
+
+    //Dagger
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation)
+    ksp(libs.hilt.android.compiler)
+
+    //Constraint
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.constraint.layout)
+
+    //Viewmodel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    //Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil3.coil.network.okhttp)
+
+    //Lottie
+    implementation(libs.lottie.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //Paginador
+    implementation(libs.androidx.paging.runtime)
+    implementation(platform(libs.androidx.compose.bom))
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+}
